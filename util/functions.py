24947807b3790e683bd569b8ff4bed1b1ae5d7ed
Andri Wild <andri.wild@students.fhnw.ch>
2023-08-10 19:24:55 +0200

    add function to ensure marker visibility when open any drawer

--- util/functions.py
+++ util/functions.py
@@ -42,3 +42,36 @@ def was_deployed(deployment: Deployment, start_date: str, end_date: str):
             return node_start < selected_start and node_end > selected_end
         else:
             return node_start <= selected_end
+
+
+def ensure_marker_visibility(map_center, map_bounds, marker_position, target_horizontal=60, target_vertical=75):
+    map_lat = map_center[0]
+    map_lon = map_center[1]
+    marker_lat = marker_position["lat"]
+    marker_lon = marker_position["lon"]
+    lower_lat = map_bounds[0][0]
+    upper_lat = map_bounds[1][0]
+    right_lon = map_bounds[0][1]
+    left_lon = map_bounds[1][1]
+
+    # lat
+    screen_height = upper_lat - lower_lat
+    marker_lat_normalized = marker_lat - lower_lat
+    percentage_marker_lat_on_screen = (100.0 / screen_height) * marker_lat_normalized
+    center_target_lat = ((screen_height / 100.0) * target_vertical) + lower_lat
+    delta_lat_move = center_target_lat - map_lat
+
+    # lon
+    screen_width = right_lon - left_lon
+    marker_lon_normalized = marker_lon - left_lon
+    percentage_marker_lon_on_screen = (100.0 / screen_width) * marker_lon_normalized
+    center_target_lon = ((screen_width / 100.0) * target_horizontal) + left_lon
+    delta_lon_move = center_target_lon - map_lon
+
+    if percentage_marker_lat_on_screen < 60:
+        map_lat = marker_lat - delta_lat_move
+
+    if percentage_marker_lon_on_screen < 20:
+        map_lon = marker_lon - delta_lon_move
+
+    return map_lat, map_lon
