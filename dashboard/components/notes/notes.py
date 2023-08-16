017b991a0d6cdba1184cc08b4b442b5ab6fd7540
Andri Wild <andri.wild@students.fhnw.ch>
2023-08-16 17:46:50 +0200

    remove notes modal content

--- dashboard/components/notes/notes.py
+++ dashboard/components/notes/notes.py
@@ -10,60 +10,6 @@ from dashboard.maindash import app
 from dashboard.util.user_validation import get_user_from_cookies
 
 
-def modal_content(data):
-    return dmc.Stack([
-        dmc.TextInput(label="Title", required=True, variant="filled"),
-        dmc.Textarea(label="Description", variant="filled"),
-        dmc.Divider(size="sm"),
-        dmc.Text("Location"),
-        dmc.Group([
-            dmc.TextInput(label="Latitude", value=data["lat"], required=True, variant="filled"),
-            dmc.TextInput(label="Longitude", value=data["lon"], required=True, variant="filled"),
-        ],
-            grow=True
-        ),
-        dcc.Upload(
-            id='upload-image',
-            children=html.Div([
-                'Drag and Drop or ',
-                html.A('Select Files')
-            ]),
-            style={
-                'width': '100%',
-                'height': '60px',
-                'lineHeight': '60px',
-                'borderWidth': '1px',
-                'borderStyle': 'dashed',
-                'borderRadius': '5px',
-                'textAlign': 'center',
-                'margin': '10px'
-            },
-            # Allow multiple files to be uploaded
-            multiple=True
-        ),
-        dmc.Button("Create", type="submit"),
-    ])
-
-
-def note_modal():
-    return dmc.Modal(
-        title="New Note",
-        id=ID_NOTE_MODAL,
-        zIndex=100000,
-        size="lg",
-        children=[
-            dmc.Text("Note")
-        ]
-    )
-
-@app.callback(
-    Output(ID_NOTE_MODAL, "opened"),
-    Output(ID_NOTE_MODAL, "children"),
-    Input(ID_NEW_NOTE_STORE, "data"),
-    prevent_initial_call=True
-)
-def open_note_modal(data):
-    return True, modal_content(data)
 
 
 
