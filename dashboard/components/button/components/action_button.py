79f0dc5c74eb19b38179c5f2dc615fb21537e089
Andri Wild <andri.wild@students.fhnw.ch>
2023-08-16 17:45:54 +0200

    add size property to action_button.py

--- dashboard/components/button/components/action_button.py
+++ dashboard/components/button/components/action_button.py
@@ -4,15 +4,15 @@ from dash_iconify import DashIconify
 from configuration import PRIMARY_COLOR
 
 
-def action_button(button_id: str = None, icon: str = None):
+def action_button(button_id: str = None, icon: str = None, size="lg"):
     return dmc.ActionIcon(
         DashIconify(
             icon=icon,
-            width=20,
+            width=20 if size == "lg" else 16,
             color=PRIMARY_COLOR,
         ),
         variant="light",
-        size="lg",
+        size=size,
         id=button_id if button_id else "",
         n_clicks=0,
         radius="xl",
