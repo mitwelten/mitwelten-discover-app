5f5ae83a6ab481ede5b007f641c3ede7e82bda4c
Andri Wild <andri.wild@students.fhnw.ch>
2023-08-17 15:44:58 +0200

    update date picker callback

--- dashboard/components/settings_drawer/components/date_time_section.py
+++ dashboard/components/settings_drawer/components/date_time_section.py
@@ -16,11 +16,17 @@ from dashboard.util.decorators import spaced_section
 @spaced_section
 def date_time_section():
     return html.Div([
-        dcc.Store(id=ID_DATE_RANGE_STORE, data=dict()),
+        dcc.Store(
+            id=ID_DATE_RANGE_STORE,
+            data=dict(
+                start=datetime.now().date() - timedelta(weeks=FIRST_DEPLOYMENT_WEEKS_AGO),
+                end=datetime.now().date()
+            )
+        ),
         dmc.SegmentedControl(
             id=ID_DATE_RANGE_SEGMENT,
             color=PRIMARY_COLOR,
-            # value=f"{FIRST_DEPLOYMENT_WEEKS_AGO}",
+            value="custom",
             fullWidth=True,
             data=DEFAULT_DATE_RANGES,
             mt=10,
@@ -34,54 +40,40 @@ def date_time_section():
                 description="",
                 minDate=date(2020, 8, 5),
                 value=[datetime.now().date() - timedelta(weeks=FIRST_DEPLOYMENT_WEEKS_AGO), datetime.now().date()],
-                style={"width": 250},
-                persistence=True
+                styles={"root": {"width": 280}},
             ),
-        ),
+        )
+
+
     ])
 
 
 @app.callback(
-    Output(ID_DATE_RANGE_STORE, "data"),
-    Output(ID_DATE_RANGE_SEGMENT, "styles", allow_duplicate=True),
-    Input(ID_DATE_RANGE_SEGMENT, "value"),
-    State(ID_APP_THEME, "theme"),
+    Output(ID_DATE_RANGE_STORE, "data", allow_duplicate=True),
+    Input(ID_DATE_RANGE_PICKER, "value"),
     prevent_initial_call=True
 )
-def update_picker_from_segment(segment_data, light_mode):
+def change_visibility_of_date_range_picker(value):
+    return dict(start=value[0], end=value[1])
+
+
+@app.callback(
+    Output(ID_DATE_RANGE_STORE, "data", allow_duplicate=True),
+    Output(ID_DATE_RANGE_PICKER, "style"),
+    Input(ID_DATE_RANGE_SEGMENT, "value"),
+    State(ID_DATE_RANGE_PICKER, "value"),
+    prevent_initial_call=True
+)
+def update_picker_from_segment(segment_data, picker_value):
     if segment_data == "":
-        return dash.no_update
+        return dash.no_update, dash.no_update
+    if segment_data == "custom":
+        return dict(start=picker_value[0], end=picker_value[1]), {"display": "block"}
 
     if not segment_data:
         seg_time_range = 7
     else:
         seg_time_range = int(segment_data)
 
-    return [datetime.now().date() - timedelta(weeks=seg_time_range), datetime.now().date()], ""
+    return dict(start=datetime.now().date() - timedelta(weeks=seg_time_range), end=datetime.now().date()), {"display": "none"}
 
-
-@app.callback(
-    Output(ID_DATE_RANGE_PICKER, "value"),
-    Output(ID_DATE_RANGE_SEGMENT, "styles", allow_duplicate=True),
-    Output(ID_DATE_RANGE_SEGMENT, "value"),
-    Input(ID_DATE_RANGE_PICKER, "value"),
-    Input(ID_DATE_RANGE_STORE, "data"),
-    State(ID_APP_THEME, "theme"),
-    State(ID_DATE_RANGE_SEGMENT, "value"),
-    prevent_initial_call=True
-)
-def update_segment_style(date_range, date_range_store, light_mode, segment_value):
-    label_color = dmc.theme.DEFAULT_COLORS['gray'][7] if light_mode else dmc.theme.DEFAULT_COLORS['gray'][1]
-    segment_styles = {
-        "active": {"visibility": "hidden", },
-        "labelActive": {"color": f"{label_color}!important"},
-    }
-
-    new_date_range = date_range
-    if dash.ctx.triggered_id == ID_DATE_RANGE_STORE:
-        segment_styles = {}
-        new_date_range = date_range_store
-    else:
-        segment_value = ""
-
-    return new_date_range, segment_styles, segment_value
