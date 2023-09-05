1cb1472db9235fae4048c29e5a13859a50749a9c
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-05 10:01:12 +0200

    refactor folder structure

--- dashboard/components/data_drawer/drawer.py
+++ dashboard/components/data_drawer/drawer.py
@@ -2,12 +2,12 @@ import dash
 import dash_mantine_components as dmc
 from dash import Output, Input, html, ALL, State
 
-from dashboard.components.data_drawer.types.devices.audio import create_audio_chart
-from dashboard.components.data_drawer.types.devices.env import create_env_chart
-from dashboard.components.data_drawer.types.devices.pax import create_pax_chart
-from dashboard.components.data_drawer.types.devices.pollinator import create_pollinator_chart
+from dashboard.components.data_drawer.types.audio import create_audio_chart
+from dashboard.components.data_drawer.types.env import create_env_chart
+from dashboard.components.data_drawer.types.note.note import create_note_view
+from dashboard.components.data_drawer.types.pax import create_pax_chart
+from dashboard.components.data_drawer.types.pollinator import create_pollinator_chart
 from dashboard.components.data_drawer.types.environment_point import create_environment_point_chart
-from dashboard.components.data_drawer.types.note import create_note_form
 from dashboard.components.notifications.notification import NotificationType, create_notification
 from dashboard.config.app import SETTINGS_DRAWER_WIDTH, DATA_SOURCES_WITHOUT_CHART_SUPPORT
 from dashboard.config.id import *
@@ -24,6 +24,7 @@ def chart_drawer():
         closeOnClickOutside=False,
         closeOnEscape=False,
         withOverlay=False,
+        withCloseButton=True,
         className="chart-drawer",
         children=[
             dmc.LoadingOverlay(
@@ -51,7 +52,7 @@ def settings_drawer_state(state):
     Output(ID_CHART_DRAWER, "opened", allow_duplicate=True),
     Output(ID_CHART_DRAWER, "position"),
     Output(ID_MAP, "center", allow_duplicate=True),
-    Input(ID_CURRENT_CHART_DATA_STORE, "data"),
+    Input(ID_CURRENT_DRAWER_DATA_STORE, "data"),
     State(ID_MAP, "bounds"),
     State(ID_MAP, "viewport"),
     prevent_initial_call=True
@@ -71,19 +72,19 @@ def open_drawer(data, bounds, viewport):
 @app.callback(
     Output(ID_CHART_CONTAINER, "children", allow_duplicate=True),
     Output(ID_NOTIFICATION_CONTAINER, "children", allow_duplicate=True),
-    Input(ID_CURRENT_CHART_DATA_STORE, "data"),
+    Input(ID_CURRENT_DRAWER_DATA_STORE, "data"),
     State(ID_ENVIRONMENT_LEGEND_STORE, "data"),
     State(ID_NOTES_STORE, "data"),
     State(ID_APP_THEME, "theme"),
     prevent_initial_call=True
 )
-def create_figure_from_store(chart_data, legend, notes, light_mode):
+def update_drawer_content_from_store(chart_data, legend, notes, light_mode):
     match chart_data["role"]:
         case "Audio Logger": chart_children = create_audio_chart(chart_data["id"], light_mode)
         case "Env. Sensor": chart_children = create_env_chart(chart_data["id"], light_mode)
         case "Pax Counter": chart_children = create_pax_chart(chart_data["id"], light_mode)
         case "Pollinator Cam": chart_children = create_pollinator_chart(chart_data["id"], light_mode)
-        case "Notes": chart_children = create_note_form(notes, chart_data["id"], light_mode)
+        case "Notes": chart_children = create_note_view(notes, chart_data["id"], light_mode)
         case "Environment Data Points": chart_children = create_environment_point_chart(legend, chart_data["id"], light_mode)
         case x: return dash.no_update, create_notification(x, "No further data available!", NotificationType.INFO)
     return chart_children, dash.no_update
