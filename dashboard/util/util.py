bee16a43b66491b0e6bcf4f647d8509aa57660fb
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-08 17:11:21 +0200

    update separator char

--- dashboard/util/util.py
+++ dashboard/util/util.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 
-def pretty_date(date, date_format="%d %b %Y - %H:%M"):
+def pretty_date(date, date_format="%d %b %Y • %H:%M"):
     return datetime.strftime(datetime.fromisoformat(date), date_format)
