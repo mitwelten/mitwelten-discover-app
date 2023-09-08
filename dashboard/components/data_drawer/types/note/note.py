d0a8d6754919617ae55dfebf9d1fa9ba543fdf0d
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-08 17:11:59 +0200

    move selected note store to app

--- dashboard/components/data_drawer/types/note/note.py
+++ dashboard/components/data_drawer/types/note/note.py
@@ -3,6 +3,9 @@ from dash import html, dcc
 
 from dashboard.components.button.components.action_button import action_button
 from dashboard.components.data_drawer.types.note import form_callbacks, attachment_callbacks, callbacks
+from dashboard.components.data_drawer.types.note.detail_view import note_detail_view
+from dashboard.components.data_drawer.types.note.form_view import note_form
+
 
 from dashboard.config.id import *
 from dashboard.model.note import Note
@@ -10,11 +13,8 @@ from dashboard.util.user_validation import get_user_from_cookies
 
 
 def drawer_content(note: Note):
-    note_dict = note.to_dict()
     user = get_user_from_cookies()
     return dmc.Container([
-            dcc.Store(ID_CURRENT_NOTE_STORE, data=dict(note_dict), storage_type="local"),
-            dcc.Store({"role": "Notes", "id": note.note_id, "label": "Prevent Marker Event"}, data=dict(state=False), storage_type="local"),
             dmc.Grid([
                 dmc.Col(dmc.Title(id=ID_NOTE_DRAWER_TITLE, order=5), span="content"),
                 dmc.Col(dmc.Group([
