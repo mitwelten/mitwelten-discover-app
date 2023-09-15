4fad4c193df21f3fc6a807bc00288b68e4cc73f4
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-15 08:01:10 +0200

    update get_identification_label function

--- dashboard/util/util.py
+++ dashboard/util/util.py
@@ -6,6 +6,9 @@ def pretty_date(date, date_format="%d %b %Y • %H:%M"):
 
 
 def get_identification_label(source) -> str:
+    if source.get('node_label') is not None:
+        return source.get('node_label')
+
     if source.get('node') is not None:
         if source.get("node").get("node_label") is not None:
             node_label = source['node']['node_label']
@@ -13,4 +16,5 @@ def get_identification_label(source) -> str:
             node_label = source['node']
     else:
         node_label = source.get("id")
+
     return node_label
