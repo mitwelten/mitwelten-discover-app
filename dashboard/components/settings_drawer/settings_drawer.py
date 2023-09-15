171e68c06e41179daad765996f913a6c88c71ce0
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-15 06:59:57 +0200

    remove map movement from settings drawer open callback

--- dashboard/components/settings_drawer/settings_drawer.py
+++ dashboard/components/settings_drawer/settings_drawer.py
@@ -1,11 +1,10 @@
 import dash_mantine_components as dmc
-from dash import Output, Input, State
+from dash import Output, Input
 
 from dashboard.components.settings_drawer.drawer_content import drawer_content
 from dashboard.config.app import SETTINGS_DRAWER_WIDTH
 from dashboard.config.id import *
 from dashboard.maindash import app
-from util.functions import ensure_marker_visibility
 
 
 def settings_drawer(deployments, tags, data_sources):
@@ -23,14 +22,8 @@ def settings_drawer(deployments, tags, data_sources):
 
 @app.callback(
     Output(ID_SETTINGS_DRAWER, "opened"),
-    Output(ID_SETTINGS_DRAWER, "position"),
-    Output(ID_MAP, "center", allow_duplicate=True),
     Input(ID_OPEN_LEFT_DRAWER_BUTTON, "n_clicks"),
-    State(ID_SELECTED_MARKER_STORE, "data"),
-    State(ID_MAP, "bounds"),
-    State(ID_MAP, "center"),
     prevent_initial_call=True,
 )
-def open_left_drawer(_, selected_marker, bounds, map_center):
-    map_center = ensure_marker_visibility(map_center, bounds, selected_marker["data"]["location"])
-    return True, "left", map_center
+def open_left_drawer(_):
+    return True
