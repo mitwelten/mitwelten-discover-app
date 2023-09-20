389e02a6534265e2fd9e475e84ee1af252128957
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-20 18:04:27 +0200

    update api url path

--- dashboard/api/api_client.py
+++ dashboard/api/api_client.py
@@ -7,7 +7,7 @@ from dashboard.config.api import API_URL
 
 
 def construct_url(path: str, args: dict = None):
-    url = f"{API_URL}{path}"
+    url = f"{API_URL}/{path}"
     if args:
         filtered_args = {k: v for k, v in args.items() if v is not None}
         if bool(filtered_args):
