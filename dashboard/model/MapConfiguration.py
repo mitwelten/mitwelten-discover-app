b84255f8099deda5029df9dae4de340c307d5cdb
Andri Wild <andri.wild@students.fhnw.ch>
2023-09-22 09:51:05 +0200

    move class MapConfiguration.py to models folder

---
+++ dashboard/model/MapConfiguration.py
@@ -0,0 +1,15 @@
+class MapConfiguration:
+
+    def __init__(
+            self,
+            title,
+            image,
+            index,
+            source,
+            source_attribution,
+    ):
+        self.title = title
+        self.image = image
+        self.index = index
+        self.source = source
+        self.source_attribution = source_attribution
