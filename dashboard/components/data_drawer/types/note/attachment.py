1c5e01f822a404db693db49bee828ae0a50f693e
Andri Wild <andri.wild@students.fhnw.ch>
2023-10-13 11:09:50 +0200

    update file property name

--- dashboard/components/data_drawer/types/note/attachment.py
+++ dashboard/components/data_drawer/types/note/attachment.py
@@ -1,22 +1,9 @@
-from pprint import pprint
-
-import dash
 import dash_mantine_components as dmc
-from dash import Output, Input, State, html, dcc, ALL, MATCH
-from dash.exceptions import PreventUpdate
-from dash_iconify import DashIconify
+from dash import html, dcc
 
-from configuration import PRIMARY_COLOR
 from dashboard.components.button.components.action_button import action_button
-from dashboard.components.data_drawer.types.note.form_view import note_form
-from dashboard.components.notifications.notification import create_notification, NotificationType
-from dashboard.config.id_config import *
-from dashboard.maindash import app
 from dashboard.model.file import File
-from dashboard.model.note import Note
 from dashboard.util.user_validation import get_user_from_cookies
-from dashboard.util.util import pretty_date
-from util.helper_functions import safe_reduce
 
 
 def attachment_table(note, is_editable=False):
@@ -41,7 +28,7 @@ def attachment_table(note, is_editable=False):
             html.Tr([
                 html.Td(file.name),
                 html.Td(file.type),
-                html.Td(file.last_modified),
+                html.Td(file.object_name),
                 html.Td(dmc.Group([
                     (action_button("id-attachment-delete-button", "material-symbols:delete", size="sm")
                      if user is not None and is_editable else {}),
