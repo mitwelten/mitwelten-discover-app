1329cff5029cb84de8f969cb28562f9f67c156ba
Andri Wild <andri.wild@students.fhnw.ch>
2023-10-13 11:03:14 +0200

    update file type to be consistent with the backend

--- dashboard/model/file.py
+++ dashboard/model/file.py
@@ -1,22 +1,19 @@
 class File:
     def __init__(self, json_file):
-        self.id = json_file.get("id")
         self.name = json_file.get("name")
         self.type = json_file.get("type")
-        self.last_modified = json_file.get("last_modified")
+        self.object_name = json_file.get("object_name")
 
     def __eq__(self, other):
         return (
-            self.id == other.id and
-            self.name == other.name and
-            self.type == other.type and
-            self.last_modified == other.last_modified
+                self.name == other.name and
+                self.type == other.type and
+                self.object_name == other.object_name
         )
 
     def to_dict(self):
         return dict(
-            id=self.id,
             name=self.name,
             type=self.type,
-            last_modified=self.last_modified
+            object_name=self.object_name
         )
