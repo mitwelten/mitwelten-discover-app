db9a6b221a930196a482e13daa0755eb757f1d39
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-08 17:19:42 +0100

    add function to upload an image

--- src/api/api_files.py
+++ src/api/api_files.py
@@ -21,5 +21,20 @@ def get_image(object_name, auth_cookie):
     encoded_image = "{}{}".format("data:image/jpg;base64, ", encoded_image)
 
     print(f"Get File from Note: object_name={object_name}, status={res.status_code}")
-    return encoded_image 
+    return encoded_image
+
+def add_file_to_note(note_id, file, auth_cookie):
+    url = construct_url(f"note/{note_id}/file")
+
+def add_file(file, name: str, auth_cookie):
+    url = construct_url(f"files/discover")
+    payload = {"file" : (name, file)}
+    res = requests.post(
+        url=url,
+        files=payload,
+        headers={"Authorization": f"Bearer {auth_cookie}"},
+    )
+    return res
+
+
 
