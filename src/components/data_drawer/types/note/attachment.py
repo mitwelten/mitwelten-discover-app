a92137ca77a3a77f691fa18a1adc0edaeafeb480
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-09 08:34:13 +0100

    add upload image callback

--- src/components/data_drawer/types/note/attachment.py
+++ src/components/data_drawer/types/note/attachment.py
@@ -1,7 +1,13 @@
+import base64
+import datetime
+import json
+
 import dash_mantine_components as dmc
-from dash import html, dcc, dash, Output, Input, State
+import flask
+from dash import html, dcc, dash, Output, Input, State, no_update, dash_table
 from dash.exceptions import PreventUpdate
 
+from src.api.api_files import add_file, add_file_to_note
 from src.main import app
 from src.config.id_config import *
 from src.components.button.components.action_button import action_button
@@ -60,20 +66,17 @@ def attachment_table(note, is_editable=False):
         },
         multiple=True
     )
-    html.Div(id='output-image-upload'),
-    return dmc.Container(
-        dmc.Stack([
-            dmc.Table(
-                header + body,
-                striped=True,
-                highlightOnHover=True,
-                withBorder=False,
-                withColumnBorders=False,
-                ),
-            drag_n_dop if is_editable else {}
-        ]
-        )
-    )
+    return dmc.Container([
+        dmc.Table(
+            header + body,
+            striped=True,
+            highlightOnHover=True,
+            withBorder=False,
+            withColumnBorders=False,
+            ),
+        drag_n_dop if is_editable else {},
+        html.Div(id="output-image-upload")
+    ])
 
 
 
@@ -82,66 +85,54 @@ def attachment_table(note, is_editable=False):
     Output(ID_ATTACHMENT_FORM_MODAL, "opened"),
     #Output("id-image-container", "children"),
     Input(ID_NOTE_ATTACHMENT_BUTTON, "n_clicks"),
-    State(ID_SELECTED_NOTE_STORE, "data"),
     prevent_initial_call=True
 )
-def update_attachment_modal_state(click, note):
+def update_attachment_modal_state(click):
     if click == 0 or click is None:
         raise PreventUpdate
 
     # auth_cookie = flask.request.cookies.get("auth")
-    # file = get_file("test_img.png", auth_cookie)
     return True
 
 
-# @app.callback(
-#     Output(ID_ALERT_INFO, "is_open", allow_duplicate=True),
-#     Output(ID_ALERT_INFO, "children", allow_duplicate=True),
-#     Input(ID_NOTE_ATTACHMENT_BUTTON, "n_clicks"),
-#     prevent_initial_call=True
-# )
-# def handle_attachment_click(click):
-#     print("handle att click")
-#     if click == 0 or click is None:
-#         raise PreventUpdate
-# 
-#     auth_cookie = flask.request.cookies.get("auth")
-#     img = get_image( "https://picsum.photos/200", auth_cookie)
-#     print("img", img)
-#     notification = [
-#         dmc.Title("Sorry, Feature not implemented yet!", order=6),
-#         dmc.Text("Attachments coming soon...")
-#     ]
-# 
-#     return True, notification
-# def parse_contents(content, name, date):
-#     return dict(content=content, name=name, date=date)
-#
-#
-# @app.callback(
-#     Output(ID_NEW_NOTE_STORE, 'data'),
-#     Input('upload-image', 'contents'),
-#     State('upload-image', 'filename'),
-#     State('upload-image', 'last_modified'),
-#     State(ID_NEW_NOTE_STORE, 'data')
-# )
-# def update_output(list_of_contents, list_of_names, list_of_dates, store):
-#     if list_of_contents is not None:
-#         content = [
-#             parse_contents(c, n, d) for c, n, d in
-#             zip(list_of_contents, list_of_names, list_of_dates)]
-#         for c in content:
-#             store.append(c)
-#     return store
-#
-#
-# @app.callback(
-#     Output('output-image-upload', "children"),
-#     Input(ID_NEW_NOTE_STORE, "data"),
-#     prevent_initial_call=True
-# )
-# def show_new_notes_from_store(data):
-#     all_note_titles = []
-#     for note in data:
-#         all_note_titles.append(html.Div(note["name"]))
-#     return all_note_titles
+def parse_contents(contents, filename):
+    return html.Div([
+        html.H5(filename),
+        html.Img(src=contents),
+        html.Hr(),
+    ])
+
+@app.callback(
+    Output("output-image-upload", "children"),
+    Input("upload-image", "contents"),
+    State("upload-image", "filename"),
+    State(ID_SELECTED_NOTE_STORE, "data")
+)
+def update_output(list_of_contents, list_of_names, note):
+    if list_of_contents is None:
+        raise PreventUpdate
+    auth_cookie = flask.request.cookies.get("auth")
+
+    uploaded_images = []
+    for content, name in zip(list_of_contents, list_of_names):
+        content_type, content_string = content.split(',')
+        decoded = base64.b64decode(content_string)
+
+        # string to split: `data:image/png;base64`
+        content_type = content_type.split(':')[1].split(';')[0]
+
+        res = add_file(decoded, name, auth_cookie)
+
+        print("1. status code: ", res.status_code)
+        print("1. return value: ", res.content)
+        if res.status_code == 200:
+            id = note["data"]["id"]
+            response_content = json.loads(res.content.decode("utf-8"))
+            obj_name = response_content["object_name"]
+            response = add_file_to_note(id, obj_name, name, content_type, auth_cookie)
+            print("2. status code: ",  response.status_code)
+            print("2. return value: ", response.content)
+            uploaded_images.append(parse_contents(content, name))
+
+    return uploaded_images
+
