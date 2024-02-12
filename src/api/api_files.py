86653fd8ac9e5134bba8b3104fb4d35b14d2a532
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-12 12:41:42 +0100

    update get_file to handle multiple object types

--- src/api/api_files.py
+++ src/api/api_files.py
@@ -1,28 +1,21 @@
 import requests
 import base64
+from pprint import pprint
 
 from src.api.api_client import construct_url
 
 
-def get_file(object_name, auth_cookie):
-    url = construct_url(f"files/discover/{object_name}")
-    res = requests.get(url=url, headers={"Authorization": f"Bearer {auth_cookie}"})
-    print(f"Get File from Note: object_name={object_name}, status={res.status_code}")
-    return None
-
-
-def get_image(object_name, auth_cookie):
-    url = construct_url(f"files/discover/{object_name}")
+def get_file(object_name, content_type, auth_cookie):
+    url = construct_url(f"files/{object_name}")
     headers={"Authorization": f"Bearer {auth_cookie}"} if auth_cookie is not None else {}
 
     res = requests.get(url=url, headers=headers)
 
     encoded_image = base64.b64encode(res.content).decode()
-    encoded_image = "{}{}".format("data:image/jpg;base64, ", encoded_image)
+    encoded_image = f"data:{content_type};base64,{encoded_image}"
 
     print(f"Get File from Note: object_name={object_name}, status={res.status_code}")
     return encoded_image
-
 
 def add_file_to_note(note_id, object_name, name, content_type, auth_cookie):
     url = construct_url(f"note/{note_id}/file")
