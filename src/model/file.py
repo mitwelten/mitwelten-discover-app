903143cee6eac21fc056dd416dae81f064a79959
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-12 12:42:08 +0100

    add file id to File

--- src/model/file.py
+++ src/model/file.py
@@ -1,11 +1,13 @@
 class File:
     def __init__(self, json_file):
+        self.id= json_file.get("id")
         self.name = json_file.get("name")
         self.type = json_file.get("type")
         self.object_name = json_file.get("object_name")
 
     def __eq__(self, other):
         return (
+                self.id == other.id and
                 self.name == other.name and
                 self.type == other.type and
                 self.object_name == other.object_name
@@ -13,6 +15,7 @@ class File:
 
     def to_dict(self):
         return dict(
+            id=self.id,
             name=self.name,
             type=self.type,
             object_name=self.object_name
