a36fe9401e1a5f772817dd9371d7d00fd15c734b
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-16 17:45:51 +0100

    add callback to get marker dragend event

--- src/assets/script.js
+++ src/assets/script.js
@@ -11,3 +11,17 @@ window.dash_clientside.browser_properties = {
     return returnObj
   },
 };
+
+
+
+window.dashExtensions = Object.assign({}, window.dashExtensions, {
+  default: {
+      function0: function(e, ctx) {
+          ctx.setProps({
+            latlng: { lat: `${e.target.getLatLng()['lat']}`,
+                      lng: `${e.target.getLatLng()['lng']}` 
+            },
+          })
+      }
+  }
+});
