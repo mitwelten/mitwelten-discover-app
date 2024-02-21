fa3b825cf007dc2253e94df5d831f49367f5bf47
Andri Wild <andri.wild@students.fhnw.ch>
2024-02-21 07:54:30 +0100

    remove old attachment implementations

--- src/components/data_drawer/types/note/attachment.py
+++ src/components/data_drawer/types/note/attachment.py
@@ -1,23 +1,15 @@
-import base64
-import json
-import uuid
-
-from enum import Enum
-from pprint import pprint
-import dash_mantine_components as dmc
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import flask
+
 from dash import  html, dcc, Output, Input, State, ctx, ALL
 from dash.exceptions import PreventUpdate
-from plotly.graph_objs import image
-from src.model.file import File
-
-from src.util.helper_functions import safe_reduce
-from src.model.note import Note
-from src.api.api_files import add_file, get_file
-from src.main import app
-from src.config.id_config import *
+from src.api.api_files import get_file
 from src.components.button.components.action_button import action_button
+from src.config.id_config import *
+from src.main import app
+from src.model.file import File
+from src.util.helper_functions import safe_reduce
 from src.util.user_validation import get_user_from_cookies
 
 
@@ -151,152 +143,6 @@ def handle_table_click(click, items, modal_state):
     carousel = dbc.Carousel(id="id-image-carousel", items=items, className="carousel-fade", variant="dark", active_index=index)
 
     return carousel
-
-
-# def create_rows(files, image_state: ImageState, is_editable, auth_cookie):
-#     rows = []
-#     for file in files:
-#         name, ext = file.object_name.split('.')
-#         thumbnail = f"{name}_thumbnail.{ext}"
-#         rows.append(
-#             html.Tr(
-#                 children=[
-#                     html.Td(
-#                         id={"element": "table", "file_id": file.id, "object_name": file.object_name, "type": file.type},
-#                         children=dmc.Image(
-#                             src=get_file(thumbnail, file.type, auth_cookie), # TODO: replace with thumbnail
-#                         withPlaceholder=True, 
-#                         width=24)),
-#                     html.Td(file.name),
-#                     html.Td(file.type),
-#                     html.Td(image_state.name),
-#                     html.Td(dmc.Group([ 
-#                         (
-#                             action_button(
-#                                 button_id={"element": "delete_button", "file_id": file.id, "object_name": file.object_name, "type": file.type}, 
-#                                 icon="material-symbols:delete", 
-#                                 size="sm") if is_editable else {}
-#                         ),
-#                         (
-#                             action_button(
-#                                 button_id={"element": "download_button", "file_id": file.id, "object_name": file.object_name, "type": file.type}, 
-#                                 icon="material-symbols:download", 
-#                                 size="sm") if image_state is ImageState.OK else {}
-#                         ),
-#                     ])),
-#                 ],
-#             )
-#         )
-#     return rows
-
-
-    # def attachment_table(note, attachment_store = None):
-    #     is_editable = note.get("inEditMode", False)
-    #     note = Note(note["data"])
-    # 
-    #     marked_to_add= attachment_store.get("add", []) if attachment_store is not None else []
-    # 
-    #     files_ok  = []
-    #     files_del = []
-    #     files_add = [File(file) for file in marked_to_add]
-    #     marked_to_delete = attachment_store.get("delete", []) if attachment_store is not None else []
-    # 
-    #     for file in note.files:
-    #         if file.id in marked_to_delete:
-    #             files_del.append(file)
-    #         else: 
-    #             files_ok.append(file)
-    # 
-    #     auth_cookie = flask.request.cookies.get("auth")
-    #     rows_ok     = create_rows(files_ok,  ImageState.OK, is_editable, auth_cookie)
-    #     rows_delete = create_rows(files_del, ImageState.TO_DELETE, is_editable, auth_cookie)
-    #     rows_add    = create_rows(files_add, ImageState.TO_ADD, is_editable, auth_cookie)
-    # 
-    # 
-    #     drag_n_dop = dcc.Upload(
-    #         id=ID_IMAGE_UPLOAD,
-    #         children="Click or Drag and Drop",
-    #         style={
-    #             'width': '100%',
-    #             'height': '60px',
-    #             'lineHeight': '60px',
-    #             'borderWidth': '1px',
-    #             'borderStyle': 'dashed',
-    #             'borderRadius': '5px',
-    #             'textAlign': 'center',
-    #             'margin': '10px'
-    #         },
-    #         multiple=True
-    #     )
-    # 
-    #     header = [
-    #         html.Thead(
-    #             html.Tr([
-    #                 html.Th(""), # Image
-    #                 html.Th("Name"),
-    #                 html.Th("Type"),
-    #                 html.Th("Status"),
-    #                 html.Th(""), # Controls
-    #             ])
-    #         )
-    #     ]
-    # 
-    #     rows = rows_ok + rows_add + rows_delete
-    #     sorted_rows = list(sorted(rows, key=lambda row: row.children[1].children))
-    # 
-    #     return dmc.Container([
-    #         drag_n_dop if is_editable else [],
-    # 
-    #         dmc.Table(
-    #             header + [html.Tbody(children=sorted_rows)],
-    #             striped=True,
-    #             highlightOnHover=True,
-    #             withBorder=False,
-    #             withColumnBorders=False,
-    #         ),
-    # 
-    #         dmc.Modal(
-    #             id=ID_IMAGE_VIEW_MODAL,
-    #             zIndex=20000,
-    #             size="full",
-    #         ),
-    # 
-    #         dcc.Download(id="id-download")
-    #     ])
-
-
-    # @app.callback(
-    #     Output(ID_IMAGE_VIEW_MODAL, "opened"),
-    #     Output(ID_IMAGE_VIEW_MODAL, "children"),
-    #     Input({"element": "table", "file_id": ALL, "object_name": ALL, "type": ALL}, "n_clicks"),
-    # )
-    # def handle_table_click(_):
-    #     print("handle_table_click")
-    #     if ctx.triggered_id is None:
-    #         raise PreventUpdate
-    #     auth_cookie = flask.request.cookies.get("auth")
-    #     return True, html.Img(src=get_file(ctx.triggered_id["object_name"], ctx.triggered_id["type"], auth_cookie))
-
-
-    # @app.callback(
-    #     Output(ID_ATTACHMENT_EDIT_MODAL, "opened", allow_duplicate=True),
-    #     Output(ID_ATTACHMENT_EDIT_MODAL, "children", allow_duplicate=True),
-    #     Input(ID_NOTE_ATTACHMENT_BUTTON, "n_clicks"),
-    #     State({"role": "Note", "label": "Store", "type": "virtual"}, "data"),
-    #     State(ID_SELECTED_NOTE_STORE, "data"),
-    #     State(ID_NOTE_ATTACHMENT_STORE, "data"),
-    #     prevent_initial_call=True
-    # )
-    # def update_attachment_modal_state(click, notes, selected_note, attachment_store):
-    #     if click == 0 or click is None:
-    #         raise PreventUpdate
-    # 
-    #     for note in notes["entries"]:
-    #          if note["id"] == selected_note["data"]["id"]:
-    #             return True, attachment_table(selected_note, attachment_store)
-    # 
-    #     # auth_cookie = flask.request.cookies.get("auth")
-    #     raise PreventUpdate
 
 
 @app.callback(
