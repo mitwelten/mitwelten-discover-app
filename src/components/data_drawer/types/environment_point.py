15a5016458f37053ff2c47f65fd705d44fdcd0cb
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-16 07:13:58 +0200

    update env data point chart

--- src/components/data_drawer/types/environment_point.py
+++ src/components/data_drawer/types/environment_point.py
@@ -2,19 +2,34 @@ from dash import dcc
 
 from src.api.api_environment import get_environment_data_by_id
 from src.components.data_drawer.charts import spider_chart
+from src.components.data_drawer.header import bottom_drawer_content
+import dash_mantine_components as dmc
 
 
-def create_environment_point_chart(legend, trigger_id, light_mode=True):
-    resp = get_environment_data_by_id(trigger_id)
+def create_environment_point_chart(legend, id, theme):
+
+    resp = get_environment_data_by_id(id)
+
     if resp is None:
         return
 
     values = [resp[key] for key in legend.keys()]
     labels = [legend[key]["label"] for key in legend.keys()]
 
-    figure = spider_chart(labels, values, light_mode)
-    return dcc.Graph(
+    figure = spider_chart(labels, values, theme)
+    graph = dcc.Graph(
         figure=figure,
         responsive=True,
         className="chart-graph",
     )
+
+    return [
+        bottom_drawer_content("Environment", "tbd", [], "habitat1.svg", theme, True), 
+        dmc.Paper(
+            children=graph,
+            shadow="md",
+            p="md",
+            radius="md",
+            style={"margin":"20px", "height":"360px"}
+        ),
+    ]
