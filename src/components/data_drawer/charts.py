47ffdee28d1f822c507e4c073873d32ef50fe00c
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-16 07:10:56 +0200

    add placeholder when no data are available

--- src/components/data_drawer/charts.py
+++ src/components/data_drawer/charts.py
@@ -1,17 +1,30 @@
 import plotly.graph_objects as go
 
-def create_themed_figure(light_mode):
+def create_themed_figure(theme):
     figure = go.Figure()
     figure.update_layout(
-        template="plotly_white" if light_mode else "plotly_dark",
+        template="plotly_white" if theme["colorScheme"] == "light" else "plotly_dark",
         paper_bgcolor="rgba(0,0,0,0)",
         plot_bgcolor="rgba(0,0,0,0)",
         margin=dict(l=30, r=30, t=5, b=30),
+        xaxis={"visible": False},
+        yaxis={"visible": False},
+        annotations=[
+            {
+                "text": "No matching data found",
+                "xref": "paper",
+                "yref": "paper",
+                "showarrow": False,
+                "font": {
+                    "size": 20 
+                }
+            }
+        ]
     )
     return figure
 
 
-def spider_chart(labels, keys, light_mode=True):
+def spider_chart(labels, keys, theme):
     fig = go.Figure()
 
     fig.add_trace(
@@ -29,7 +42,7 @@ def spider_chart(labels, keys, light_mode=True):
     )
 
     fig.update_layout(
-        template="plotly_white" if light_mode else "plotly_dark",
+        template="plotly_white" if theme["colorScheme"] == "light" else "plotly_dark",
         polar=dict(
             radialaxis=dict(
                 # gridcolor="grey"
