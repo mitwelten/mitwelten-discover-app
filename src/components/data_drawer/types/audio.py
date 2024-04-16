c2d4d16b63027f956207166e80f8f2973231c42e
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-16 07:10:02 +0200

    update audio chart to plot the most 3 species

--- src/components/data_drawer/types/audio.py
+++ src/components/data_drawer/types/audio.py
@@ -1,30 +1,104 @@
 from datetime import datetime, timedelta
 
+from pprint import pprint
+from plotly.colors import hex_to_rgb
 import plotly.graph_objects as go
 from dash import dcc
+import dash_mantine_components as dmc
+from src.config.app_config import SECONDARY_COLOR
+from src.model.deployment import Deployment
 
-from src.api.api_deployment import get_audio_timeseries
+from src.api.api_deployment import get_audio_timeseries, get_audio_top3
+from src.components.data_drawer.header import bottom_drawer_content
 from src.components.data_drawer.charts import create_themed_figure
 
 
-def create_audio_chart(trigger_id, light_mode=True):
+def create_audio_chart(deployment_data, theme):
+
+    d = Deployment(deployment_data)
     resp = get_audio_timeseries(
         taxon_id=212,
-        deployment_id=trigger_id,
+        deployment_id=d.id,
         bucket_width="1d",
         time_from=(datetime.now() - timedelta(days=1000)).isoformat(),
         time_to=datetime.now().isoformat(),
         confidence=0.9
     )
-    figure = create_themed_figure(light_mode)
-    figure.add_trace(go.Bar(
-        x=resp["bucket"],
-        y=resp["detections"],
-    ))
+
+    figure = create_themed_figure(theme)
+
+    if resp is not None and len(resp["bucket"]) != 0:
+        figure.update_layout(
+            annotations=[{"visible":False}],
+            xaxis={"visible": True},
+            yaxis={"visible": True},
+        )
+        figure.add_trace(go.Bar(
+            x=resp["bucket"],
+            y=resp["detections"],
+        ))
+
     graph = dcc.Graph(
         figure=figure,
         responsive=True,
         className="chart-graph",
 
     )
-    return graph
+    return [
+        bottom_drawer_content("Audio", "tbd", d.tags, "audio.svg", theme, True), 
+        dmc.Paper(
+            children=graph,
+            shadow="md",
+            p="md",
+            radius="md",
+            style={"margin":"20px", "height":"360px"}
+        ),
+    ]
+
+def create_audio_chart2(deployment_data, theme):
+
+    d = Deployment(deployment_data)
+    resp = get_audio_top3(deployment_id=d.id)
+
+    figure = create_themed_figure(theme)
+
+    if resp is not None and len(resp) != 0:
+        figure.update_layout(
+            annotations=[{"visible":False}],
+            xaxis={"visible": True},
+            yaxis={"visible": True},
+        )
+
+        x=[value["count"] for value in resp]
+        y=[value["species"] for value in resp]
+        x.reverse()
+        y.reverse()
+        
+                
+        figure.add_trace(go.Bar(
+            x=x,
+            y=y,
+            orientation="h",
+            opacity=0.8,
+            marker=dict(
+                color=f"rgb{hex_to_rgb(theme['colors'][SECONDARY_COLOR][4])}",
+                line=dict(color=f"rgb{hex_to_rgb(theme['colors'][SECONDARY_COLOR][-1])}", width=1)
+            )
+        ))
+
+    graph = dcc.Graph(
+        figure=figure,
+        responsive=True,
+        className="chart-graph",
+
+    )
+    return [
+        bottom_drawer_content("Audio", "tbd", d.tags, "audio.svg", theme, True), 
+        dmc.Paper(
+            children=graph,
+            shadow="md",
+            p="md",
+            radius="md",
+            style={"margin":"20px", "height":"360px"}
+        ),
+    ]
