b8e0295db173d8b5989e955afa2b5fa0f7277850
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-16 07:13:19 +0200

    update env chart

--- src/components/data_drawer/types/env.py
+++ src/components/data_drawer/types/env.py
@@ -2,6 +2,8 @@ import dash_mantine_components as dmc
 import plotly.graph_objects as go
 from dash import dcc
 
+from src.model.deployment import Deployment
+from src.components.data_drawer.header import bottom_drawer_content
 from src.api.api_deployment import get_env_timeseries
 from src.components.data_drawer.charts import create_themed_figure
 from src.config.api_config import *
@@ -12,6 +14,15 @@ def create_env_temp_chart(trigger_id, bucket_width, light_mode):
     temp = get_env_timeseries(trigger_id, "temperature", "mean", bucket_width)
     temp = cleanup_timeseries(temp, TEMP_LOWER_BOUNDARY, TEMP_UPPER_BOUNDARY)
     figure = create_themed_figure(light_mode)
+
+    if len(temp["value"]) == 0:
+        return figure
+
+    figure.update_layout(
+        annotations=[{"visible":False}],
+        xaxis={"visible": True},
+        yaxis={"visible": True},
+    )
     figure.add_trace(go.Scatter(
         x=temp["time"],
         y=temp["value"],
@@ -23,10 +34,20 @@ def create_env_hum_chart(trigger_id, bucket_width, light_mode):
     hum = get_env_timeseries(trigger_id, "humidity", "mean", bucket_width)
     hum = cleanup_timeseries(hum, HUM_LOWER_BOUNDARY, HUM_UPPER_BOUNDARY)
     figure = create_themed_figure(light_mode)
+
+    if len(hum["value"]) == 0:
+        return figure
+
+    figure.update_layout(
+        annotations=[{"visible":False}],
+        xaxis={"visible": True},
+        yaxis={"visible": True},
+    )
     figure.add_trace(go.Scatter(
         x=hum["time"],
         y=hum["value"],
     ))
+
     return figure
 
 
@@ -34,6 +55,16 @@ def create_env_moi_chart(trigger_id, bucket_width, light_mode):
     moi = get_env_timeseries(trigger_id, "moisture", "mean", bucket_width)
     moi = cleanup_timeseries(moi, MOI_LOWER_BOUNDARY, MOI_UPPER_BOUNDARY)
     figure = create_themed_figure(light_mode)
+
+    if len(moi["value"]) == 0:
+        return figure
+
+
+    figure.update_layout(
+        annotations=[{"visible":False}],
+        xaxis={"visible": True},
+        yaxis={"visible": True},
+    )
     figure.add_trace(go.Scatter(
         x=moi["time"],
         y=moi["value"],
@@ -41,7 +72,8 @@ def create_env_moi_chart(trigger_id, bucket_width, light_mode):
     return figure
 
 
-def create_env_chart(trigger_id, light_mode=True):
+def create_env_chart(deployment_data, theme):
+    d = Deployment(deployment_data)
     bucket_width = "1h"
     tabs = [
         dict(
@@ -62,7 +94,7 @@ def create_env_chart(trigger_id, light_mode=True):
 
     def create_graph(fn):
         return dcc.Graph(
-            figure=fn(trigger_id, bucket_width, light_mode),
+            figure=fn(d.id, bucket_width, theme),
             responsive=True,
             className="chart-graph",
         )
@@ -76,9 +108,20 @@ def create_env_chart(trigger_id, light_mode=True):
 
     tab_list = [dmc.Tab(f"{tab['title']}", value=f"{idx}") for (idx, tab) in enumerated_tabs]
 
-    return dmc.Tabs([dmc.TabsList(tab_list, position="center"), *panel_list],
+    tabs = dmc.Tabs([dmc.TabsList(tab_list, position="center"), *panel_list],
                     value="0",
                     persistence=True,
                     variant="outline",
-                    style={"height": "90%"}
-                    )
+                    style={"height": "90%"})
+
+
+    return [
+        bottom_drawer_content("Environment Sensor", "tbd", d.tags, "thermo.svg", theme, True), 
+        dmc.Paper(
+            children=tabs,
+            shadow="md",
+            p="md",
+            radius="md",
+            style={"margin":"20px", "height":"360px"}
+        ),
+    ]
