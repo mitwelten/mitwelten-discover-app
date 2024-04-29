990bae86571be42b25f7b395ca949254bec19d1e
Andri Wild <andri.wild@students.fhnw.ch>
2024-04-29 11:31:07 +0200

    add audio top 3 bar chart

--- src/components/data_drawer/types/audio.py
+++ src/components/data_drawer/types/audio.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
 
-from pprint import pprint
 from plotly.colors import hex_to_rgb
 import plotly.graph_objects as go
+import pandas as pd
 from dash import dcc
 import dash_mantine_components as dmc
-from src.config.app_config import SECONDARY_COLOR
+from src.config.app_config import AUDIO_DESCRIPTION, SECONDARY_COLOR
 from src.model.deployment import Deployment
 
-from src.api.api_deployment import get_audio_timeseries, get_audio_top3
+from src.api.api_deployment import get_audio_timeseries, get_audio_top3, get_bird_stacked_bar
 from src.components.data_drawer.header import bottom_drawer_content
 from src.components.data_drawer.charts import create_themed_figure
 
@@ -94,6 +94,57 @@ def create_audio_chart2(deployment_data, theme):
     )
     return [
         bottom_drawer_content("Audio", "tbd", d.tags, "audio.svg", theme, True), 
+        dmc.Paper(
+            children=graph,
+            shadow="md",
+            p="md",
+            radius="md",
+            style={"margin":"20px", "height":"360px"}
+        ),
+    ]
+
+def create_audio_chart3(deployment_data, date_range, theme):
+
+    d = Deployment(deployment_data)
+    resp = get_bird_stacked_bar(
+        deployment_id=d.id,
+        time_from=date_range["start"],
+        time_to=date_range["end"],
+        bucket_width="1d",
+        confidence=0.9,
+    )
+
+    figure = create_themed_figure(theme)
+
+    if resp is not None and len(resp) != 0:
+        figure.update_layout(
+            annotations=[{"visible":False}],
+            xaxis={"visible": True},
+            yaxis={"visible": True},
+        )
+
+        df = pd.DataFrame(resp)
+
+        bars = []
+        for group, dfg in df.groupby(by='species'):
+            x=dfg['bucket']
+            y=dfg['count']
+            bars.append(go.Bar(name=group,x=x, y=y))
+
+        timeseries = pd.bdate_range(date_range["start"], date_range["end"], tz="UTC", freq="D")
+
+        figure.add_traces(bars)
+        figure.add_traces(go.Bar(x=timeseries, y=[0] * len(timeseries)))
+        figure.update_layout(barmode='stack')
+                
+    graph = dcc.Graph(
+        figure=figure,
+        responsive=True,
+        className="chart-graph",
+
+    )
+    return [
+        bottom_drawer_content("Audio", AUDIO_DESCRIPTION, d.tags, "test/audio.svg", theme, True), 
         dmc.Paper(
             children=graph,
             shadow="md",
