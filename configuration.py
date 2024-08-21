e4c7f6592d3f296934be3fbc0118ab69f1d4df1a
Andri Wild <andri.wild@students.fhnw.ch>
2024-08-21 16:20:49 +0200

    add configuration

---
+++ configuration.py
@@ -0,0 +1,12 @@
+# KC_SERVER_URL = os.environ['KC_SERVER_URL']
+# KC_CLIENT_ID = os.environ['KC_CLIENT_ID']
+# KC_REALM_NAME = os.environ['KC_REALM_NAME']
+
+KC_SERVER_URL = "https://auth.mitwelten.org/auth/"
+KC_CLIENT_ID = "discover"
+KC_REALM_NAME = "mitwelten"
+DOMAIN_NAME = "https://discover.mitwelten.org"
+#DOMAIN_NAME = "http://localhost:8000"
+
+API_URL = "https://data.mitwelten.org/api/v3"
+#API_URL = "http://localhost:8001"
