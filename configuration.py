d3c437c51a69b53b60825f211fe29b23cbe31808
Andri Wild <andri.wild@students.fhnw.ch>
2024-08-21 16:49:16 +0200

    update configuration

--- configuration.py
+++ configuration.py
@@ -1,12 +1,13 @@
-# KC_SERVER_URL = os.environ['KC_SERVER_URL']
-# KC_CLIENT_ID = os.environ['KC_CLIENT_ID']
-# KC_REALM_NAME = os.environ['KC_REALM_NAME']
+import os
 
-KC_SERVER_URL = "https://auth.mitwelten.org/auth/"
-KC_CLIENT_ID = "discover"
-KC_REALM_NAME = "mitwelten"
-DOMAIN_NAME = "https://discover.mitwelten.org"
-#DOMAIN_NAME = "http://localhost:8000"
+# run locally
+if os.path.exists("./.env"):
+    from dotenv import load_dotenv
+    load_dotenv(".env")
 
 API_URL = "https://data.mitwelten.org/api/v3"
-#API_URL = "http://localhost:8001"
+
+KC_SERVER_URL = os.environ["KC_SERVER_URL"]
+KC_CLIENT_ID  = os.environ["KC_CLIENT_ID"]
+KC_REALM_NAME = os.environ["KC_REALM_NAME"]
+DOMAIN_NAME   = os.environ["DOMAIN_NAME"]
